a8198dd9293ac8cc18fdffd68389b772b1764df3
Behrad Tahamtani <beviseta@outlook.com>
2023-04-17 17:19:01 +0200

    update savepath of CSV files

--- RunTests/dynamicWarehouse.bat
+++ RunTests/dynamicWarehouse.bat
@@ -3,7 +3,7 @@ cls
 Set Sleep=0
 :start
 if %Sleep% == 20 ( goto end )
-roslaunch ../test.launch goal_x:=100 goal_y:=100 fileName:="dynamic_logistics_Warehouse_%Sleep%.csv" worldFile:="$(find dynamic_logistics_warehouse)/launch/logistics_warehouse.launch"
+roslaunch ../test.launch goal_x:=100 goal_y:=100 fileName:="DynamicWarehouse/dynamic_logistics_Warehouse_%Sleep%.csv" worldFile:="$(find dynamic_logistics_warehouse)/launch/logistics_warehouse.launch"
 Set Sleep+=1
 echo %Sleep%
 goto start
